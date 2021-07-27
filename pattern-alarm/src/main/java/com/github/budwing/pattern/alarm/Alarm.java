786fe1d79988eea331390658539acadbf7cb2b3a
budwing <budwing@sina.com>
2021-07-27 21:28:36 +0800

    Add thread pool and retry ability

--- pattern-alarm/src/main/java/com/github/budwing/pattern/alarm/Alarm.java
+++ pattern-alarm/src/main/java/com/github/budwing/pattern/alarm/Alarm.java
@@ -4,22 +4,25 @@ import com.github.budwing.pattern.alarm.device.AlarmDevice;
 import com.github.budwing.pattern.alarm.message.AlarmMessage;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
-public class Alarm {
+public class Alarm implements Callable<Boolean>, Comparable<Alarm> {
 	private AlarmMessage message;
 	private AlarmDevice device;
 	private List<Employee> contacts;
+	private int alarmLevel;
 	private int retryTimes;
 	
 	public Alarm(AlarmMessage message, AlarmDevice device,
-			List<Employee> contacts, int retryTimes) {
+			List<Employee> contacts, int alarmLevel, int retryTimes) {
 		super();
 		this.message = message;
 		this.device = device;
 		this.contacts = contacts;
+		this.alarmLevel = alarmLevel;
 		this.retryTimes = retryTimes;
 	}
-	
+
 	public int getRetryTimes() {
 		return retryTimes;
 	}
@@ -34,12 +37,21 @@ public class Alarm {
 		}
 	}
 	
-	public boolean execute() {
+	public void decreaseRetryTimes() {
+		retryTimes--;
+	}
+	
+	public Boolean call() {
 		message.setDevice(device);
 		boolean result = message.sendTo(contacts);
 		
 		log(contacts, 1, message.build(), String.valueOf(result));
 		
 		return result;
+	}
+
+	public int compareTo(Alarm o) {
+		// TODO Auto-generated method stub
+		return this.alarmLevel - o.alarmLevel;
 	}
 }
--- pattern-alarm/src/main/java/com/github/budwing/pattern/alarm/AlarmExecutor.java
+++ pattern-alarm/src/main/java/com/github/budwing/pattern/alarm/AlarmExecutor.java
@@ -1,14 +1,33 @@
 package com.github.budwing.pattern.alarm;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class AlarmExecutor {
+	private PriorityBlockingQueue<Alarm> queue = new PriorityBlockingQueue<Alarm>();
+	private ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+	
 	public void execute(Alarm alarm) {
-		for (int i = 0; i < alarm.getRetryTimes(); i++) {
-			if (alarm.execute()) {
-				break;
+		queue.offer(alarm);
+	}
+
+	public void start() {
+		while(true) {
+			Alarm alarm = queue.poll();
+			Future<Boolean> result = threadPool.submit(alarm);
+			try {
+				if(!result.get() && alarm.getRetryTimes()>0) {
+					alarm.decreaseRetryTimes();
+					queue.offer(alarm);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
-
 
 	/*public static void main(String[] args) {
 		Alarm a = new Alarm();
