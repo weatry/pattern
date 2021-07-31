25e6abc904c89a1e28ae362796291daf4815ec86
budwing <budwing@sina.com>
2021-07-31 19:22:14 +0800

    Extract amountFor method from statement

--- pattern-rental/src/main/java/com/github/budwing/pattern/rental/Customer.java
+++ pattern-rental/src/main/java/com/github/budwing/pattern/rental/Customer.java
@@ -26,25 +26,7 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		
 		for(Rental rental:rentals) {
-			double thisAmount = 0;
-			
-			switch(rental.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if(rental.getDaysRented()>2) {
-					thisAmount += (rental.getDaysRented()-2)*1.5;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += rental.getDaysRented()*3;
-				break;
-			case Movie.CHILDREN:
-				thisAmount += 1.5;
-				if(rental.getDaysRented()>3) {
-					thisAmount += (rental.getDaysRented()-3)*1.5;
-				}
-				break;
-			}
+			double thisAmount = amountFor(rental);
 			
 			frequentRenterPoints++;
 			
@@ -60,5 +42,28 @@ public class Customer {
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		
 		return result;
+	}
+
+	private double amountFor(Rental rental) {
+		double thisAmount = 0;
+		
+		switch(rental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if(rental.getDaysRented()>2) {
+				thisAmount += (rental.getDaysRented()-2)*1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += rental.getDaysRented()*3;
+			break;
+		case Movie.CHILDREN:
+			thisAmount += 1.5;
+			if(rental.getDaysRented()>3) {
+				thisAmount += (rental.getDaysRented()-3)*1.5;
+			}
+			break;
+		}
+		return thisAmount;
 	}
 }
