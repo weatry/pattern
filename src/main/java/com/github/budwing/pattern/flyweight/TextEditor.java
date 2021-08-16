0195e0795d3843d49e4c6192d2d06ff72d8de70d
budwing <budwing@sina.com>
2021-08-16 16:06:35 +0800

    Update flyweight

--- src/main/java/com/github/budwing/pattern/flyweight/TextEditor.java
+++ src/main/java/com/github/budwing/pattern/flyweight/TextEditor.java
@@ -1,17 +1,18 @@
 package com.github.budwing.pattern.flyweight;
 
+import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TextEditor {
-	private Map<Character,Letter> cache = new HashMap<Character,Letter>();
+	private Map<Character,SoftReference<Letter>> cache = new HashMap<Character,SoftReference<Letter>>();
 	
 	public Letter getLetter(char c) {
-		Letter result = cache.get(c);
-		if(result==null) {
-			result = new Letter(c);
-			cache.put(c, result);
+		SoftReference<Letter> result = cache.get(c);
+		if(result==null || result.get()==null) {
+			result = new SoftReference(new Letter(c));
+			cache.put(c, (result));
 		}
-		return result;
+		return result.get();
 	}
 }
